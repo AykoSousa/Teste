307e91ba7295610bd609629dea59fc654c2ccab3
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2024-02-22 21:00:47 -0300

    Some changes

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ let btn = document.getElementById("btn");
 let title = document.getElementById("Label__yJo9rgMwM");
 
 const handleClick = () => {
+    title.innerHTML = "Clicou";
     console.log("Clicou");
 }
